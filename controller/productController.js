757eaa3378ef1bbd53449ec55be6a173dd2be0d4
HuuLocFIT <nguyenhuuloc7654@gmail.com>
2022-12-07 09:42:00 +0700

    add filter products by manufacture, price range and sort products by price, release date

--- controller/productController.js
+++ controller/productController.js
@@ -1,22 +1,29 @@
-const {getAllProducts, getSaleProducts,getSpecificProductById,getRelatedProduct,getProductsByName,getProductsByCategory} = require('../services/productService')
+const {getAllProducts, getSaleProducts,getSpecificProductById,getRelatedProduct,getProductsByName,getProductsByCategory,getProductsByManufacture} = require('../services/productService')
 const qs = require('qs')
 const getListProduct = async(req, res, next)=>{
     const {nameProduct} = req.query;
     const {category} = req.query
     const {sort, ...withoutSort} = req.query;
+    const {manufacture} = req.query;
+    const {minPrice, maxPrice} = req.query;
     let listProducts = [];
-    if(nameProduct){
-        listProducts = await getProductsByName(nameProduct)
-    }else if(category){
-        listProducts = await getProductsByCategory(category)
-    }else{
 
-        listProducts = await getAllProducts();
+    if(nameProduct){
+        listProducts = await getProductsByName(nameProduct, sort, minPrice, maxPrice)
+    }else if(category){
+        listProducts = await getProductsByCategory(category, sort, minPrice, maxPrice)
+    }
+    else if(manufacture) {
+        listProducts = await getProductsByManufacture(manufacture, sort, minPrice, maxPrice)
+    } 
+    else {
+        listProducts = await getAllProducts(sort, minPrice, maxPrice)
     }
     
 
     const saleProducts = await getSaleProducts();
     res.render('products',{listProducts, saleProducts, originalUrl: `${req.baseUrl}?${qs.stringify(withoutSort)}`})
+
 }
 const showDetail = async(req, res, next)=>{
      const id = req.params.id;
@@ -24,6 +31,5 @@ const showDetail = async(req, res, next)=>{
      const relatedProducts = await getRelatedProduct(id)
      res.render('productDetail', {productItem,relatedProducts});
  }
- 
 
 module.exports = {getListProduct, showDetail}
--- services/productService.js
+++ services/productService.js
@@ -1,8 +1,23 @@
+const e = require('express');
 const db = require('../db/index')
 
-const getAllProducts = async()=>{
+const getAllProducts = async(sort, minPrice, maxPrice)=>{
+    let result = null;
 
-    const result = await db.connection.execute('SELECT * FROM product');;
+    if(minPrice && maxPrice) {
+        result = await db.connection.execute('SELECT * FROM product where product.price >= ? and product.price <= ?', [minPrice, maxPrice]);
+        
+        if(sort === 'price')
+            result = await db.connection.execute('SELECT * FROM product where product.price >= ? and product.price <= ? order by product.price asc', [minPrice, maxPrice]);
+        else if(sort === 'releasedate')
+            result = await db.connection.execute('SELECT * FROM product where product.price >= ? and product.price <= ? order by product.create_at asc', [minPrice, maxPrice]);
+    }
+    else if(sort === 'price')
+        result = await db.connection.execute('SELECT * FROM product order by product.price asc');
+    else if(sort === 'releasedate')
+        result = await db.connection.execute('SELECT * FROM product order by product.create_at asc');
+    else
+        result = await db.connection.execute('SELECT * FROM product');
     return result[0]
 }
 
@@ -20,16 +35,48 @@ const getRelatedProduct = async(id) => {
     const result = await db.connection.execute('select * from product where product.id != ? and product.category in (select product.category from product  where product.id = ?)', [id,id])
     return result[0]
 }
-const getProductsByName = async(name)=>{
-    const result = await db.connection.execute("select * from product where product.name like ? ",[`%${name}%`]);
+const getProductsByName = async(name, sort, minPrice, maxPrice)=> {
+    let result = null;
+    
+    if(minPrice && maxPrice) 
+        result = await db.connection.execute('SELECT * FROM product where product.price >= ? and product.price <= ?', [minPrice, maxPrice]);
+    else if(sort === 'price')
+        result = await db.connection.execute('SELECT * FROM product where product.name like ? order by product.price asc',[`%${name}%`]);
+    else if(sort === 'releasedate')
+        result = await db.connection.execute('select * from product where product.name like ? oder by product.create_at',[`%${name}%`]);
+    else
+        result = await db.connection.execute('select * from product where product.name like ? ',[`%${name}%`]);
     return result[0]
 }
 
-const getProductsByCategory = async(category)=>{
-    const result = await db.connection.execute("select * from product where product.category like ? ",[`%${category}%`]); 
+const getProductsByCategory = async(category, sort, minPrice, maxPrice)=>{
+    let result = null;
+
+    if(minPrice && maxPrice) 
+        result = await db.connection.execute('SELECT * FROM product where product.price >= ? and product.price <= ?', [minPrice, maxPrice]);
+    else if(sort === 'price')
+        result = await db.connection.execute('SELECT * FROM product where product.category like ? order by product.price asc',[`%${category}%`]);
+    else if(sort === 'releasedate')
+        result = await db.connection.execute('select * from product where product.category like ? oder by product.create_at',[`%${category}%`]);
+    else
+        result = await db.connection.execute("select * from product where product.category like ? ",[`%${category}%`]); 
     return result[0];
 }
 
-module.exports = {getAllProducts, getSaleProducts, getSpecificProductById,getRelatedProduct,getProductsByName,getProductsByCategory}
+const getProductsByManufacture = async(manufacture, sort, minPrice, maxPrice)=>{ 
+    let result = null;
+
+    if(minPrice && maxPrice) 
+        result = await db.connection.execute('SELECT * FROM product where product.price >= ? and product.price <= ?', [minPrice, maxPrice]);
+    if(sort === 'price')
+        result = await db.connection.execute("select * from product where product.manufacture like ? order by product.price asc",[`%${manufacture}%`]);
+    else if(sort === 'releasedate')
+        result = await db.connection.execute("select * from product where product.manufacture like ? order by product.create_at asc",[`%${manufacture}%`]);
+    else 
+        result = await db.connection.execute("select * from product where product.manufacture like ? ",[`%${manufacture}%`]);
+    return result[0]; 
+}
+
+module.exports = {getAllProducts, getSaleProducts, getSpecificProductById,getRelatedProduct,getProductsByName,getProductsByCategory,getProductsByManufacture}
 
 
