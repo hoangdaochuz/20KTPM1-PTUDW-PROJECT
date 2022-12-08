0b90a0db10e23c027679d7391e987aeb0b9bd5c4
hoangdaochuz <74745917+hoangdaochuz@users.noreply.github.com>
2022-12-08 20:55:05 +0700

    host database to aws for public access

--- db/index.js
+++ db/index.js
@@ -5,10 +5,10 @@ const db = {connection: null};
 (async () => {
   // create the connection to database
   db.connection = await mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    // password: '280502',
-    password: 'huuloc123',
+    host: 'db-cloud-ptudw.czomp6y0wfdy.ap-southeast-1.rds.amazonaws.com',
+    user: 'hkdarealest',
+    password: 'Nguyenhuukhai2805',
+    // password: 'huuloc123',
     database: 'ktpm1-ptudw-project'
   });
   console.log('Database connected!');
