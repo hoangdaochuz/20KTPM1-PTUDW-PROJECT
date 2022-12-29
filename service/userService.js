5024176f2f174b1e07d398f91096117285055187
HuuLocFIT <nguyenhuuloc7654@gmail.com>
2022-12-30 02:05:41 +0700

    update change profile admin

--- service/userService.js
+++ service/userService.js
@@ -7,15 +7,15 @@ const getInfoUser = async() => {
 
 const updateInfoUser = async(id, fullName, avatarImage) => {
     result = null
-    console.log(avatarImage)
+    console.log("FullName: " + fullName)
 
-    if(id !== undefined && fullName !== undefined && avatarImage === "")
+    if(id !== undefined && avatarImage === "/locasset/images/user/") {
         result = await db.connection.execute('UPDATE user SET user.full_name = ? WHERE user.id = ?', [`${fullName}`, `${id}`])
-    else if(id && avatarImage !== "") {
-        result = await db.connection.execute('UPDATE user SET user.avatar = ? WHERE user.id = ?', [`${avatarImage}`, `${id}`])
     }
-
-    return result;
+    else if(id && avatarImage !== "/locasset/images/user/") {
+        result = await db.connection.execute('UPDATE user SET user.avatar = ?, user.full_name = ? WHERE user.id = ?', [`${avatarImage}`, `${fullName}`, `${id}`])
+    }
+        return result;
 }
 
 module.exports = {getInfoUser, updateInfoUser}
